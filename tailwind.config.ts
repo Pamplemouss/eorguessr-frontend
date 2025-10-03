19eb940a32926fa666dd3693099f332913392397
John <john.dufaye@gmail.com>
2025-10-03 11:32:54 +0200

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-const plugin = require('tailwindcss/plugin')
 
 const config: Config = {
 	content: [
@@ -16,23 +15,8 @@ const config: Config = {
 				background: "var(--background)",
 				foreground: "var(--foreground)",
 			},
-			textShadow: {
-				DEFAULT: '1px 0px 2px var(--tw-shadow-color), -1px 0px 2px var(--tw-shadow-color), 0px 1px 2px var(--tw-shadow-color), 0px -1px 2px var(--tw-shadow-color)',
-			},
 		},
 	},
-	plugins: [
-		require('tailwindcss-text-border'),
-		plugin(function ({ matchUtilities, theme }: { matchUtilities: any, theme: any }) {
-			matchUtilities(
-				{
-					'text-shadow': (value: string) => ({
-						textShadow: value,
-					}),
-				},
-				{ values: theme('textShadow') }
-			)
-		}),
-	],
+	plugins: [],
 };
 export default config;
